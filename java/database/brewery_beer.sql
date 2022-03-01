ad72905c6d2f65beced6d4d017f758217a9ea4d2
Chexpeare <cguevara@nj.rr.com>
2022-03-01 18:49:33 -0500

    First commit: set up back-end repository and database

---
+++ java/database/brewery_beer.sql
@@ -0,0 +1,9 @@
+BEGIN;
+
+
+INSERT INTO brewery_beer (brewery_id, beer_id)
+VALUES (1,1);
+
+
+COMMIT;
+
--- java/database/create.sh
+++ java/database/create.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 BASEDIR=$(dirname $0)
-DATABASE=final_capstone
+DATABASE=final_capstone_chexpeare
 psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres $DATABASE &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql"
+psql -U postgres -d $DATABASE -f "$BASEDIR/table_init.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql" &&
+#! psql -U postgres -d $DATABASE -f "$BASEDIR/data.sql"
+psql -U postgres -d $DATABASE -f "$BASEDIR/breweries.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/beers.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/reviews.sql" 
+#! psql -U postgres -d $DATABASE -f "$BASEDIR/brewery_beer.sql" 
