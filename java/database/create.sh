43d8c3840a0b3072ea175ce2197360ef48ef1dfc
Chexpeare <cguevara@nj.rr.com>
2022-03-03 17:35:44 -0500

    Added Jdbc files

--- java/database/create.sh
+++ java/database/create.sh
@@ -4,10 +4,11 @@ DATABASE=final_capstone
 psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres $DATABASE &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/table_init.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql" &&
-#! psql -U postgres -d $DATABASE -f "$BASEDIR/data.sql"
+psql -U postgres -d $DATABASE -f "$BASEDIR/table_init.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/breweries.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/beers.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/reviews.sql" 
-#! psql -U postgres -d $DATABASE -f "$BASEDIR/brewery_beer.sql" 
+
+#! psql -U postgres -d $DATABASE -f "$BASEDIR/data.sql"
+#! data.sql is a backup file for all the table data.
