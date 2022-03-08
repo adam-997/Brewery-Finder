5a973862c3f7fbc8e1bdb3fbc51c2b0d2e0f7961
Adam Aguilar <adamaguilar997@gmail.com>
2022-03-07 21:21:26 -0600

    added headers

--- frontend/src/Components/Main/Main.js
+++ frontend/src/Components/Main/Main.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
-import { Switch, Route, Redirect, Link } from "react-router-dom";
+import { Switch, Route, Redirect, Link, NavLink } from "react-router-dom";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
 import Home from "../Home/Home";
 import { addToken, deleteUser } from "../../Redux/actionCreators";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
+import { Layout, Menu, Breadcrumb } from "antd";
+import icon from "../../image/Logo3.png";
+const { Header, Content, Footer } = Layout;
 
 const mapStateToProps = (state) => {
   return {
@@ -37,15 +40,50 @@ class Main extends Component {
     return (
       <div>
         {this.props.token.token !== undefined ? (
-          <div>
-            <Link to="/home">Home | </Link>
-            <Link to="/login" onClick={this.handleLogout}>
-              logout
-            </Link>
-            <Redirect to="/home" />
-          </div>
+          <Layout>
+            <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+              <div className="logo" src={icon}></div>
+              <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+                <Menu.Item key="2">
+                  <NavLink to="/home" activeClassName="active">
+                    Home{" "}
+                  </NavLink>
+                </Menu.Item>
+                <Menu.Item key="1">
+                  {" "}
+                  <NavLink to="/login" onClick={this.handleLogout}>
+                    logout
+                  </NavLink>
+                </Menu.Item>
+                <Menu.Item key="3">nav 3</Menu.Item>
+                <Redirect to="/home" />
+              </Menu>
+            </Header>
+            <Content
+              className="site-layout"
+              style={{ padding: "0 50px", marginTop: 64 }}
+            >
+              <Breadcrumb style={{ margin: "16px 0" }}>
+                <Breadcrumb.Item>Home</Breadcrumb.Item>
+              </Breadcrumb>
+            </Content>
+          </Layout>
         ) : (
-          <Link to="/login">Home | </Link>
+          <Layout>
+            <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+              <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+                <Menu.Item key="2">
+                  <NavLink to="/home" activeClassName="active">
+                    Home{" "}
+                  </NavLink>
+                </Menu.Item>
+              </Menu>
+            </Header>
+            <Content
+              className="site-layout"
+              style={{ padding: "0 50px", marginTop: 64 }}
+            ></Content>
+          </Layout>
         )}
         <Switch>
           <Route path="/login" component={() => <Login />} />
