e77e0566289f7303e09bcedf616608f5656f428d
Adam Aguilar <adamaguilar997@gmail.com>
2022-03-10 21:57:35 -0600

    changed stlyes for reviews

--- frontend/src/Components/Breweries/Beer.js
+++ frontend/src/Components/Breweries/Beer.js
@@ -32,42 +32,8 @@ function RenderReviewCard({
   description,
   rating,
 }) {
-  const [likes, setLikes] = useState(0);
-  const [dislikes, setDislikes] = useState(0);
-  const [action, setAction] = useState(null);
-  const like = () => {
-    setLikes(1);
-    setDislikes(0);
-    setAction("liked");
-  };
-
-  const dislike = () => {
-    setLikes(0);
-    setDislikes(1);
-    setAction("disliked");
-  };
-
-  const actions = [
-    <Tooltip key="comment-basic-like" title="Like">
-      <span onClick={like}>
-        {createElement(action === "liked" ? LikeFilled : LikeOutlined)}
-        <span className="comment-action">{likes}</span>
-      </span>
-    </Tooltip>,
-    <Tooltip key="comment-basic-dislike" title="Dislike">
-      <span onClick={dislike}>
-        {React.createElement(
-          action === "disliked" ? DislikeFilled : DislikeOutlined
-        )}
-        <span className="comment-action">{dislikes}</span>
-      </span>
-    </Tooltip>,
-    <span key="comment-basic-reply-to">Reply to</span>,
-  ];
-
   return (
     <Comment
-      actions={actions}
       author={<a>Beer Lover</a>}
       avatar={
         <Avatar
@@ -77,17 +43,8 @@ function RenderReviewCard({
           alt="Beer Lover"
         />
       }
-      content={
-        <p>
-          {description}
-          <br /> {rating}
-        </p>
-      }
-      datetime={
-        <Tooltip title={moment().format("YYYY-MM-DD HH:mm:ss")}>
-          <span>{moment().fromNow()}</span>
-        </Tooltip>
-      }
+      content={<p>{description}</p>}
+      datetime={<Text style={{ fontWeight: "bold" }}> {rating + " / 10"}</Text>}
     />
   );
 }
